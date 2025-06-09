a6f736388942d1167a5e7b7f1eafd1c9a19dddc7
hari123-dot <srihariks1602@gmail.com>
2025-06-09 12:44:39 +0530

    triale

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
             <h1>This is Srihari.</h1>
             <h3>I am Web developer.</h3>
             <h5>Currently studying in Itech instiute of technology.</h5>
-            
+            <h5>I am working in React.js and Vanilla js</h5>
         </div>
     );
 };
