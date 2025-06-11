b216e020237fc6c9c730b3a5d9bd3fd142e06f37
hari123-dot <srihariks1602@gmail.com>
2025-06-11 19:27:40 +0530

    Changes Updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,18 @@ import "./Footer.css";
 const Footer = () => {
     return (
         <div className="footer">
-            <p> This is the Footer</p>
+         <p> Hi, I'm Sri Hari  — Itech Of Cmputers.</p>
+<p>Explore:</p>
+<p> - Home</p>
+<p>- About</p>
+<p>- Projects</p>
+<p>- Contact</p>
+
+<p>Subscribe to get the latest articles: [Email Field]</p>
+
+<p>© 2025 Sri Hari. Powered by react.</p>  
+  <p>[GitHub]</p>
+
 
         </div>
     );
